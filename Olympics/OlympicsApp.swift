1644b017995abd7b6d6433ac9e4ee59f8966fc6b
RickQuino <rquino0001@gmail.com>
2026-04-29 12:43:10 -0700

    Initial Commit

---
+++ Olympics/OlympicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OlympicsApp.swift
+//  Olympics
+//
+//  Created by QUINO, RICK on 4/29/26.
+//
+
+import SwiftUI
+
+@main
+struct OlympicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
